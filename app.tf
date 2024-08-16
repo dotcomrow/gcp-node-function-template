3a25a04f6bc0da2ea162cfa7763a2c49f3932abb
mephmanx <52037738+mephmanx@users.noreply.github.com>
2024-08-16 20:06:48 +0000

    Deploying to release from @ dotcomrow/gcp-logging-processor@7eefb9422f5aa333a414dde4f81c46973fad418f 🚀

---
+++ app.tf
@@ -0,0 +1,76 @@
+resource "random_id" "bucket_prefix" {
+  byte_length = 8
+}
+
+
+resource "google_service_account" "default" {
+  account_id   = var.project_id
+  display_name = "${var.project_id}-service-account"
+}
+
+resource "google_pubsub_topic" "default" {
+  name = "${var.project_id}-topic"
+}
+
+resource "google_storage_bucket" "default" {
+  name                        = "${random_id.bucket_prefix.hex}-${var.project_id}-source-bucket" # Every bucket name must be globally unique
+  location                    = "US"
+  uniform_bucket_level_access = true
+}
+
+data "archive_file" "default" {
+  type        = "zip"
+  output_path = "/tmp/function-source.zip"
+  source_dir  = "src/"
+}
+
+resource "google_storage_bucket_object" "default" {
+  name   = "function-source.zip"
+  bucket = google_storage_bucket.default.name
+  source = data.archive_file.default.output_path # Path to the zipped function source code
+}
+
+resource "google_cloudfunctions2_function" "default" {
+  name        = "${var.project_id}-function"
+  location    = var.region
+  description = "${var.project_id}-function"
+
+  build_config {
+    runtime     = "nodejs18"
+    entry_point = "onMessage" # Set the entry point
+    # environment_variables = {
+    #   BUILD_CONFIG_TEST = "build_test"
+    # }
+    source {
+      storage_source {
+        bucket = google_storage_bucket.default.name
+        object = google_storage_bucket_object.default.name
+      }
+    }
+  }
+
+  service_config {
+    max_instance_count = 1
+    min_instance_count = 0
+    available_memory   = "128M"
+    timeout_seconds    = 60
+    
+    environment_variables = {
+      GCP_BIGQUERY_PROJECT_ID      = var.GCP_BIGQUERY_PROJECT_ID
+      SHARED_SERVICE_ACCOUNT_EMAIL = var.SHARED_SERVICE_ACCOUNT_EMAIL
+      GCP_LOGGING_CREDENTIALS      = var.GCP_LOGGING_CREDENTIALS
+      GCP_LOGGING_PROJECT_ID       = var.GCP_LOGGING_PROJECT_ID
+    }
+
+    ingress_settings               = "ALLOW_INTERNAL_ONLY"
+    all_traffic_on_latest_revision = true
+    service_account_email          = google_service_account.default.email
+  }
+
+  event_trigger {
+    trigger_region = var.region
+    event_type     = "google.cloud.pubsub.topic.v1.messagePublished"
+    pubsub_topic   = google_pubsub_topic.default.id
+    retry_policy   = "RETRY_POLICY_RETRY"
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,55 @@
+import * as ff from '@google-cloud/functions-framework';
+import { serializeError } from "serialize-error";
+import { GCPLogger } from "npm-gcp-logging";
+import { GCPAccessToken } from "npm-gcp-token";
+
+interface PubSubData {
+  subscription: string;
+  message: {
+    messageId: string;
+    publishTime: string;
+    data: string;
+    attributes?: {[key: string]: string};
+  };
+}
+
+ff.cloudEvent<PubSubData>('TypescriptFunction', async ce => {
+  try {
+    console.log(ce.data?.message.messageId);
+  } catch (e) {
+    if (!process.env.GCP_LOGGING_CREDENTIALS) {
+      console.log("GCP_LOGGING_CREDENTIALS is not defined");
+      return;
+    }
+    
+    if (!process.env.GCP_LOGGING_PROJECT_ID) {
+      console.log("GCP_LOGGING_PROJECT_ID is not defined");
+      return;
+    }
+
+    if (!process.env.K_SERVICE) {
+      console.log("K_SERVICE is not defined");
+      return;
+    }
+
+    var logging_token = await new GCPAccessToken(
+      process.env.GCP_LOGGING_CREDENTIALS
+    ).getAccessToken("https://www.googleapis.com/auth/logging.write");
+    const responseError = serializeError(e);
+      await GCPLogger.logEntry(
+        process.env.GCP_LOGGING_PROJECT_ID,
+        logging_token.access_token,
+        process.env.K_SERVICE,
+        [
+          {
+            severity: "ERROR",
+            // textPayload: message,
+            jsonPayload: {
+              responseError,
+            },
+          },
+        ]
+      );
+    return;
+  }
+});
